1bb0c5e8727be1a8a5c0dd215ab1f5a7e90a96c8
mlaurasouza <mlaura2souza@gmail.com>
2021-02-10 01:26:24 +0000

    (09/02) Início da revisão de JS

---
+++ revisao-js/desestruturacao.js
@@ -0,0 +1,17 @@
+let vetor = [10, 20, 30]
+
+let obj ={ 
+    nome: 'Maria', 
+    idade: 34,
+    naturalidade: 'Franca/SP'
+}
+
+let [x, y, z] = vetor
+console.log(x)
+console.log(y)
+console.log(z)
+
+let {nome, idade, naturalidade} = obj
+console.log(nome)
+console.log(idade)
+console.log(naturalidade)
